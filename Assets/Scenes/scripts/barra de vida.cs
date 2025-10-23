cc61417f01c2622f70bfbbeccfe29265a2dfe648
marcelodisjsb-lang <marcelodisjsb@gmail.com>
2025-10-23 18:49:33 -0300

    Commit 23-10 version3

---
+++ Assets/Scenes/scripts/barra de vida.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraDeVida : MonoBehaviour
+{
+    public Image vidaAtualImage;
+
+    public void AtualizarUI(float calculoVida)
+    {
+        Vector3 novaEscala = new Vector3(calculoVida, 1, 1);
+        vidaAtualImage.transform.localScale = novaEscala;
+    }
+}
